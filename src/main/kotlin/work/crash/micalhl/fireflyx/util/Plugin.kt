aed05318494c2eb2a04d5ed93c9b1ea0065e1172
Micalhl <micalhl@sina.cn>
2022-07-09 23:34:24 +0800

    update
    - [*] 我是傻逼.

--- src/main/kotlin/work/crash/micalhl/fireflyx/util/Plugin.kt
+++ src/main/kotlin/work/crash/micalhl/fireflyx/util/Plugin.kt
@@ -5,5 +5,8 @@ import taboolib.module.nms.nmsClass
 fun getTPS(): List<Double> {
     val craftServer = nmsClass("MinecraftServer").getMethod("getServer").invoke(null)
     val recentTps = craftServer.javaClass.getField("recentTps")
-    return cast<DoubleArray>(recentTps.get(craftServer)).onEach { "%.1f".format(it) }.map { it }
+    val cast = cast<DoubleArray>(recentTps.get(craftServer))
+    val result = arrayListOf<Double>()
+    cast.forEach { result.add("%.1f".format(it).toDouble()) }
+    return result
 }
